92b4b703e4dcab01de1cb6ac162c262403f3a6dc
Michael Steigman <michael@steigman.net>
2006-01-04 20:39:31 +0000

    duh... bad comment in upgrade script

--- sql/postgresql/upgrade/upgrade-0.9.6b-0.9.7.sql
+++ sql/postgresql/upgrade/upgrade-0.9.6b-0.9.7.sql
@@ -1,4 +1,4 @@
-# Create a few indexes to speed up index page query
+-- Create a few indexes to speed up index page query
 create index na_saved_items_aggregator_id_idx on na_saved_items(aggregator_id);
 create index na_purges_aggregator_id_idx on na_purges(aggregator_id);
 create index na_aggregators_package_id_idx on na_aggregators(package_id);
