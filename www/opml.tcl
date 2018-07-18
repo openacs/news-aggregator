3749c48616942c2d80929c0a9bf1808425cd8bfb
Antonio Pisano <antonio@elettrotecnica.it>
2018-07-18 17:08:51 +0000

    Give some feedback in case of error

--- www/opml.tcl
+++ www/opml.tcl
@@ -7,10 +7,13 @@ ad_page_contract {
     {aggregator_id:naturalnum,notnull}
 }
 
-if { [catch {set opml [news_aggregator::aggregator::as_opml \
-			-aggregator_id $aggregator_id]} errmsg] } {
-    doc_return 200 text/html ""
-} else {
-    doc_return 200 application/xml $opml
+if {[catch {
+    set opml [news_aggregator::aggregator::as_opml \
+                  -aggregator_id $aggregator_id]
+} errmsg]} {
+    ad_return_error "OPML generation error" "OPML generation error: $errmsg"
+    ad_script_abort
 }
+
+ns_return 200 application/xml $opml
 ad_script_abort
