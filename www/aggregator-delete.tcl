111f2a19b37f07c41f5ab1a1f173aebe353bfc3b
Antonio Pisano <antonio@elettrotecnica.it>
2021-10-05 12:04:22 +0000

    Bring portable SQL inline

--- www/aggregator-delete.tcl
+++ www/aggregator-delete.tcl
@@ -11,12 +11,24 @@ permission::require_permission \
     -privilege write
 
 set user_id [ad_conn user_id]
-set default_aggregator [db_string find_default {}]
+set default_aggregator [db_string find_default {
+    select default_aggregator
+    from   na_user_preferences
+    where  user_id = :user_id
+}]
 
 if {$delete_aggregator_id eq $default_aggregator} {
     # We are deleting the user's default aggregator
     # Set user's oldest aggregator as new default
-    set new_default_aggregator [db_string select_oldest_aggregator {} -default ""]
+    set new_default_aggregator [db_string select_oldest_aggregator {
+        select min(aggregator_id)
+        from   na_aggregators a,
+               acs_objects o
+        where  a.aggregator_id = o.object_id
+        and    creation_user = :user_id
+        and    aggregator_id != :delete_aggregator_id
+        order  by creation_date
+    } -default ""]
     if { $new_default_aggregator ne "" } {
         news_aggregator::aggregator::set_user_default \
             -user_id $user_id \
